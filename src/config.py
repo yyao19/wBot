a475d426841c5fe139c5df63096490786f24320f
yaoyuan4102 <yaoyuan4102@gmail.com>
2017-07-28 22:01:14 -0500

    add baidu speech recognition

--- src/config.py
+++ src/config.py
@@ -1,3 +1,10 @@
 TULING_API_KEY = u'7c5fea5075ca4e0ca31a38a4427e02d8'
-MAX_HISTORY = 1000
+TULING_URL = u'http://www.tuling123.com/openapi/api'
+MAX_HISTORY = 10000
 MESSAGE_LENGTH = 150
+
+BAIDU_VOICE = {
+    u'app_id': u'9937303',
+    u'api_key': u'94VvjGW0gpSg8rOQCcHYpLau',
+    u'secret_key': u'36572195e9b0e0c9d976499b25da7c18'
+}
--- src/wBot.py
+++ src/wBot.py
@@ -1,21 +1,35 @@
 # -*- coding: utf-8 -*-
 from wxpy import *
+from aip import AipSpeech
 import config as config
+import os
+import requests
+import random
 import re
 
 
 class WBot(object):
     def __init__(self):
         self.bot = Bot(True, 2)
+        self.bot.enable_puid()
         self.bot.messages.max_history = config.MAX_HISTORY
         self.tuling = Tuling(api_key=config.TULING_API_KEY)
+        self.aipSpeech = AipSpeech(
+            config.BAIDU_VOICE[u'app_id'],
+            config.BAIDU_VOICE[u'api_key'],
+            config.BAIDU_VOICE[u'secret_key']
+        )
+        self.session = requests.Session()
+        self.tulingUrl = config.TULING_URL
 
     def run(self):
         self.auto_reply_tuling_robot()
         self.auto_accept_friends()
         self.recalled_message_helper()
+        self.voice_message_helper()
         embed()
 
+    ### Message Listener ###
     def auto_reply_tuling_robot(self):
         """
         注册图灵机器人聊天对象
@@ -73,6 +87,34 @@ class WBot(object):
                         text_msg = recalled_msg.sender.nick_name + u' 撤回了一条消息'
                     msg.reply(text_msg)
 
+    ### Multimedia Message Process ###
+    def voice_message_helper(self):
+        """
+        对语音消息进行处理
+        
+        :return: 
+        """
+        @self.bot.register(msg_types=RECORDING)
+        def auto_process(msg):
+            if isinstance(msg.chat, Group):
+                return
+            else:
+                file_path = self._download_attachment(msg, RECORDING)
+                if file_path:
+                    audio_path = self._audio_conversion(file_path)
+                    if os.path.isfile(audio_path):
+                        response = self.aipSpeech.asr(self._get_file_content(audio_path), 'wav', 8000, {
+                            'lan': 'zh',
+                        })
+                        res_msg = self._next_topic()
+                        if response[u'err_no'] == 0:
+                            text = response[u'result'][0]
+                            user_id = re.sub(r'[^a-zA-Z\d]', '', msg.sender.user_name)
+                            answer = self._tuling_msg(message=text, user_id=user_id)
+                            res_msg = self._process_answer(answer)
+                        msg.reply(res_msg)
+
+    ### Helper Function ###
     def _search_message_by_id(self, message_id):
         """
         从缓存历史消息中根据消息ID查询消息
@@ -96,3 +138,110 @@ class WBot(object):
             msg = msg[0:config.MESSAGE_LENGTH] + u'...'
         return msg
 
+    def _download_attachment(self, msg, msg_type):
+        """
+        下载文件到对应文件夹
+        
+        :param msg: 
+        :param msg_type: 
+        :return: 
+        """
+        file_path = self._get_storage_path(msg, msg_type)
+        msg.get_file(save_path=file_path)
+        if os.path.isfile(file_path) and os.path.getsize(file_path) > 0:
+            return file_path
+        else:
+            return None
+
+    @staticmethod
+    def _get_storage_path(msg, msg_type):
+        """
+        Get File Path
+        
+        :param msg: 
+        :param msg_type: 
+        :return: 
+        """
+        puid = msg.sender.puid if msg.sender.puid else u'other'
+        path = u'attachment/' + msg_type.lower() + u'/' + puid
+        absolute_path = os.getcwd()
+        if path:
+            path_list = path.split(u'/')
+            for folder in path_list:
+                absolute_path = absolute_path + u'/' + folder
+                if not os.path.exists(absolute_path):
+                    os.makedirs(absolute_path)
+            absolute_path = absolute_path + u'/' + msg.file_name
+        return absolute_path
+
+    @staticmethod
+    def _audio_conversion(path, audio_format='.wav', sample_rate='8000'):
+        """
+        Audio Conversion
+        
+        :param path: 
+        :return: 
+        """
+        filename, file_extension = os.path.splitext(path)
+        audio_path = filename + audio_format
+        conversion_command = u'ffmpeg -i ' + path + u' -acodec pcm_s16le -ar ' + sample_rate + u' ' + audio_path
+        os.system(conversion_command)
+
+        return audio_path
+
+    @staticmethod
+    def _get_file_content(file_path):
+        with open(file_path, 'rb') as fp:
+            return fp.read()
+
+    ### Tuling Function ###
+    def _tuling_msg(self, message, user_id):
+        """
+        向Tuling发送消息
+        :param message: 
+        :param user_id: 
+        :return: 
+        """
+        payload = dict(
+            key=config.TULING_API_KEY,
+            info=message,
+            userid=user_id,
+            loc=None
+        )
+        # noinspection PyBroadException
+        try:
+            r = self.session.post(self.tulingUrl, json=payload)
+            answer = r.json()
+        except:
+            answer = None
+
+        return answer
+
+    def _process_answer(self, answer):
+        """
+        解析Tuling返回消息
+        
+        :param answer: 
+        :return: 
+        """
+        ret = self._next_topic()
+        code = -1
+        if answer:
+            code = answer.get('code', -1)
+
+        if code >= 100000:
+            text = answer.get('text')
+            if text:
+                ret = text
+        return ret
+
+    @staticmethod
+    def _next_topic():
+        """
+        聊天机器人无法获取回复时的备用回复
+        """
+
+        return random.choice((
+            u'小主人，我听不清楚呢！',
+            u'你再说一遍嘛'
+        ))
